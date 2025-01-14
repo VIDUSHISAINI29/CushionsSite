1a64e0a9bdf6b7c8f88cf8c0ded83a11270deebe
VIDUSHISAINI29 <vidushisaini29@gmail.com>
2025-01-14 22:01:53 +0530

    Frontend Design of Cushions Website Initialization

---
+++ src/layout/AppLayout.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Navbar from "./Navbar.vue";
+</script>
+
+<template>
+   <div>
+      <Navbar />
+   </div>
+</template>
+
+<style scoped></style>
---
+++ src/layout/Navbar.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+<template>
+   <div class="tw-flex tw-justify-between tw-bg-purple-300 tw-p-1">
+      <div class="tw-flex tw-gap-2 tw-bg-red-300">
+         <i class="ri-search-line tw-bg-yellow-900 tw-text-xl"></i>
+         <input class="tw-outline-none" type="text" placeholder="Search..." />
+      </div>
+   </div>
+</template>
