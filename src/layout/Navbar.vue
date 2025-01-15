785a14ddac7c7976e1db341cfc84236486d24f42
VIDUSHISAINI29 <vidushisaini29@gmail.com>
2025-01-15 23:55:42 +0530

    Navbar edited

--- src/layout/Navbar.vue
+++ src/layout/Navbar.vue
@@ -1,9 +1,20 @@
 <script setup></script>
 <template>
-   <div class="tw-flex tw-justify-between tw-bg-purple-300 tw-p-1">
-      <div class="tw-flex tw-gap-2 tw-bg-red-300">
-         <i class="ri-search-line tw-bg-yellow-900 tw-text-xl"></i>
-         <input class="tw-outline-none" type="text" placeholder="Search..." />
+   <div
+      class="tw-flex tw-w-full tw-items-center tw-justify-around tw-p-1 tw-shadow-lg">
+      <div class="tw-flex tw-gap-2 tw-border tw-border-yellow-900 tw-p-2">
+         <i class="ri-search-line tw-text-xl tw-text-yellow-900"></i>
+         <input
+            class="tw-text-yellow-900 tw-outline-none"
+            type="text"
+            placeholder="Search..." />
       </div>
+      <div class="tw-flex tw-gap-5">
+         <span class="tw-bg-red-300">Home</span>
+         <span class="tw-bg-red-300">Shop</span>
+         <span class="tw-bg-red-300">About</span>
+         <span class="tw-bg-red-300">Contact</span>
+      </div>
+      <div class=""></div>
    </div>
 </template>
